8991c3b9adb81cf0f12815b8084ee364303f1aa3
Harsh Tiwari <109919683+Harsh1820@users.noreply.github.com>
2023-05-31 23:46:40 +0530

    first commit

---
+++ removeDuplicatefromUnsortedLL.cpp
@@ -0,0 +1,22 @@
+
+#include<bits/stdc++.h>
+Node *removeDuplicates(Node *head)
+{	
+    Node* curr = head;
+    Node* prev = NULL;
+    unordered_map<int, int> visited;
+    
+    while(curr != NULL){
+        if(!visited[curr->data]){
+            visited[curr->data] = 1;
+            prev = curr;
+            curr = curr -> next;
+        }
+        else{
+            prev -> next = curr -> next;
+            delete curr;
+        }
+        curr = prev -> next;
+    }
+    return head;
+}
