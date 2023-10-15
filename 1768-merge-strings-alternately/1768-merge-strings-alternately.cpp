13c080b78737669533607ffc0805e81857a04a1e
Harsh Tiwari <109919683+Harsh1820@users.noreply.github.com>
2023-10-15 23:59:41 +0530

    Time: 3 ms (49.78%), Space: 6.6 MB (49.97%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,36 +1,38 @@
-// class Solution {
-// public:
-//     string mergeAlternately(string word1, string word2) {
-//         string result = "";
-//         int i = 0;
-//         while (i < word1.length() || i < word2.length()) {
-//             if (i < word1.length()) {
-//                 result += word1[i];
-//             }
-//             if (i < word2.length()) {
-//                 result += word2[i];
-//             }
-//             i++;
-//         }
-//         return result;
-//     }
-// };
-
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string ans="";int j;int i;
-        for(i=0,j=0;i<word1.size() && j<word2.size();i++,j++){
-            ans=ans+word1[i];
-            ans=ans+word2[j];
-           
+        string result = "";
+        int i = 0;
+        int j=0;
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length()) {
+                result += word1[i];
+            }
+            if (j < word2.length()) {
+                result += word2[j];
+            }
+            i++;
+            j++;
         }
-        if(i!=word1.length()){
-            ans+=word1.substr(i,word1.size()-i);
-        }
-         if(j!=word2.length()){
-            ans+=word2.substr(j,word2.size()-j);
-        }
-        return ans;
+        return result;
     }
 };
+
+// class Solution {
+// public:
+//     string mergeAlternately(string word1, string word2) {
+//         string ans="";int j;int i;
+//         for(i=0,j=0;i<word1.size() && j<word2.size();i++,j++){
+//             ans=ans+word1[i];
+//             ans=ans+word2[j];
+           
+//         }
+//         if(i!=word1.length()){
+//             ans+=word1.substr(i,word1.size()-i);
+//         }
+//          if(j!=word2.length()){
+//             ans+=word2.substr(j,word2.size()-j);
+//         }
+//         return ans;
+//     }
+// };
