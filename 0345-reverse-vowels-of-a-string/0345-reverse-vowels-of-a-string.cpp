79d3c94af9373816799bdf0e0271596f559dff25
Harsh Tiwari <109919683+Harsh1820@users.noreply.github.com>
2023-10-21 23:02:10 +0530

    Time: 6 ms (67.28%), Space: 8.2 MB (30.90%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -20,9 +20,7 @@ public:
             }
             if(isVowel(s[left]) && isVowel(s[right]))
             {
-                char temp = s[left];
-                s[left] = s[right];
-                s[right] = temp;
+                swap(s[left], s[right]);
                 left++;
                 right--;
             }
@@ -31,3 +29,18 @@ public:
         return s;
     }
 };
+
+// 2nd approach 
+
+// class Solution {
+// public:
+//     string reverseVowels(string s) {
+//         int i=0; int j= s.size()-1;
+//         while (i<j){
+//             if (!(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'))i++;
+//             if (!(s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o' || s[j]=='u' || s[j]=='A' || s[j]=='E' || s[j]=='I' || s[j]=='O' || s[j]=='U'))j--;
+//             if ((s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U') && (s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o' || s[j]=='u' || s[j]=='A' || s[j]=='E' || s[j]=='I' || s[j]=='O' || s[j]=='U')){swap(s[i],s[j]);i++; j--;}
+//         }
+//         return s;
+//     }
+// };
